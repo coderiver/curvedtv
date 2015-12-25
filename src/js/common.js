61e3a633bb8f503f25de3d90a85da4327a985b31
whats0n <mrdvv@mail.ru>
2015-12-25 14:54:44 +0200

    update

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,47 @@
 $(document).ready(function() {
 
-	// go js
+	$(document).click(function() {
+		$(".js-select").removeClass("is-active");
+		$(".js-select-list").slideUp(100);
+	});
+		
+	// select list
+	$("body").on("click",".js-select",function(event) {
+		event.stopPropagation();
+	});
+	$("body").on("click",".js-select-text",function(event) {
+		var select = $(this).parents(".js-select");
+		if (select.hasClass("is-active")) {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+		}
+		else {
+			$(".js-select").removeClass("is-active");
+			$(".js-select-list").slideUp(100);
+			select.toggleClass("is-active").find(".js-select-list").slideToggle(100);
+		}
+
+	});
+
+	$("body").on("click",".js-select-list li",function() {
+		var val = $(this).attr("data-val");
+		var text = $(this).text();
+		var select = $(this).parents(".js-select");
+		var selectList = $(this).parents(".js-select-list");
+		select.find(".js-select-text").text(text);
+		select.find("option").removeAttr("selected");
+		select.find('option[value="'+val+'"]').attr("selected", "selected");
+		select.addClass("is-chosen");
+		select.removeClass("is-active");
+		selectList.slideUp(100);
+		return false;
+		
+	});
+	$('select').on('change', function () {
+		var val = $(this).find('option:selected').val();
+		if($(window).width() < 768) {
+			$(this).siblings(".js-select-text").text(val);
+		}
+	});
 
 });
